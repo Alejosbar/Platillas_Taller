d426095b80fa87497070e0fa0514c0b9ac5d5dae
Alejosbar <yazocult@hotmail.com>
2025-08-14 14:27:05 -0500

    Carga inicial de archivos

---
+++ contact/script.js
@@ -0,0 +1,26 @@
+// Initialize tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Theme switcher
+const themeToggle = document.getElementById('theme-toggle');
+const htmlEl = document.documentElement;
+
+const setTheme = (theme) => {
+    htmlEl.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    if (theme === 'dark') {
+        themeToggle.innerHTML = '<i class="bi bi-sun-fill"></i>';
+    } else {
+        themeToggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
+    }
+};
+
+const savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+setTheme(savedTheme);
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = htmlEl.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+});
---
+++ ecommerce/script.js
@@ -0,0 +1,33 @@
+// Initialize Toasts
+const toastTrigger = document.getElementById('applyDiscountBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+  })
+}
+
+// Theme switcher
+const themeToggle = document.getElementById('theme-toggle');
+const htmlEl = document.documentElement;
+
+const setTheme = (theme) => {
+    htmlEl.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    if (theme === 'dark') {
+        themeToggle.innerHTML = '<i class="bi bi-sun-fill"></i>';
+    } else {
+        themeToggle.innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
+    }
+};
+
+const savedTheme = localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+setTheme(savedTheme);
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = htmlEl.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+});
+// Add any custom JavaScript here
